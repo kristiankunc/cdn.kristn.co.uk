9ec17a07e31a217bfd48de5b938514c5ec719c81
KristN <kristian.kunc@gmail.com>
2022-02-20 23:02:17 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
 import random
 import string
-from waitress import serve
-from flask import Flask, jsonify, request, redirect
+from flask import Flask, render_template, request, jsonify
+from firebase_admin import credentials, initialize_app, storage
 
 app = Flask(__name__)
+cred = credentials.Certificate("./creds/kristn-cdn-firebase-adminsdk.json")
+initialize_app(cred, {"storageBucket": "kristn-cdn.appspot.com"})
 
-with open("./token.txt", "r") as f:
+with open("./creds/token.txt", "r") as f:
     token = f.read()
 
-@app.route("/")
-def index():
-    return redirect("https://kristn.tech")
-
 @app.route("/upload", methods=["POST"])
-def upload():
-    if "token" in request.form and "file" in request.files:
-        user_token = request.headers["token"]
-        file = request.files["file"]
+def index():
+    if "Authorization" in request.headers and "file" in request.files:
+        user_token = request.headers["Authorization"]
+        uploaded_file = request.files["file"]
 
         if user_token == token:
-            file.save("./storage/" + ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(5)))
-            return jsonify({"success": True})
+            
+            filename = "".join(random.choice(string.ascii_lowercase + string.digits) for _ in range(5))
+
+            bucket = storage.bucket()
+            blob = bucket.blob(filename)
+            blob.upload_from_file(uploaded_file, content_type=uploaded_file.content_type)
+            
+            blob.make_public()
+
+            return jsonify({"success": True, "filename": filename, "file_url": blob.public_url, "url": f"https://cdn.kristn.co.uk/i/{filename}"})
 
         else:
             return jsonify({"message": "403 Forbidden, invalid token"}), 403
     else:
-        return jsonify({"message": "403 Forbidden, token or file missing"}), 403
+        return jsonify({"message": "400 Forbidden, token or file missing"}), 400
 
+@app.route("/i/<filename>", methods=["GET"])
+def image(filename):
+    return render_template("embed.html", filename=filename, url=f"https://cdn.kristn.co.uk/i/{filename}", img_url=f"https://storage.googleapis.com/kristn-cdn.appspot.com/{filename}")
+
+    
 if __name__ == "__main__":
-    port = 5000
-
-    print(f"serving on port {port}")
-    serve(app, port=port)
+    from waitress import serve
+    print(f"Running\nhttp://localhost:5000")
+    serve(app, port=5000)
