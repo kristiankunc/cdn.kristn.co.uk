ca68ed5b859b7a6ab242a786ee1dcf519916a83a
Kristián Kunc <kristian.kunc@gmail.com>
2022-08-25 17:19:11 +0200

    disable json key sorting

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, jsonify, redirect
 from firebase_admin import credentials, initialize_app, storage
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
 cred = credentials.Certificate("./creds/kristn-cdn-firebase-adminsdk.json")
 initialize_app(cred, {"storageBucket": "kristn-cdn.appspot.com"})
 
